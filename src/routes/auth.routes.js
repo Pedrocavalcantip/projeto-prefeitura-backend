18b85635f68e463acd62b101a896c86c6ac5787e
felipeccoutinho06 <felipeccoutinho8708@gmail.com>
2025-07-06 17:23:37 -0300

    Ajustei

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { login } = require('../Controllers/authController.js'); // Importa o controlador de login
+const { login } = require('../controllers/auth.controller.js'); // Importa o controlador de login
 const verificarToken = require('../middlewares/authMiddleware.js'); // Importa o middleware de proteção
 
 // Rota pública de login
