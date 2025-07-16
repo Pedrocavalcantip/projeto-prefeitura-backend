e6e60b67be052cf2f23cf856961af964737fd3b3
felipeccoutinho06 <felipeccoutinho8708@gmail.com>
2025-07-16 13:39:03 -0300

    Fiz esses testes usando esse jest e usando mock, que me permite testar sem precisar de um banco de dados, só checando se as funcionalidades estão funcionando

---
+++ __mocks__/prisma.js
@@ -0,0 +1,16 @@
+module.exports = {
+  produtos: {
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  },
+  realocacoes_produto: {
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  }
+};
