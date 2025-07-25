faae6869160e6422bc5f8162376477facbb7ac91
pedrocavalcantip <pfcc@cin.ufpe.br>
2025-07-25 19:38:12 -0300

    mudando o index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const dotenv            = require('dotenv');
 
 dotenv.config();
 
-require('./src/jobs/doacoes.job');
+require('./src/jobs/doacoes.job.js');
 require('./src/jobs/realocacoesJob.js');
 
 
-const authRouter        = require('./src/routes/auth.routes');
-const doacoesRouter     = require('./src/routes/doacoes.routes');
-const realocacoesRoutes = require('./src/routes/realocacoes.routes');
+const authRouter        = require('./src/routes/auth.routes.js');
+const doacoesRouter     = require('./src/routes/doacoes.routes.js');
+const realocacoesRoutes = require('./src/routes/realocacoes.routes.js');
 
 const app = express();
 app.use(express.json());
