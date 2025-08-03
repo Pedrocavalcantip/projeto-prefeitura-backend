02b5a65aa37a36be7fb913395036222681d05030
pedrocavalcantip <pfcc@cin.ufpe.br>
2025-08-03 10:15:06 -0300

    adicionando cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const multer = require('multer');
 
@@ -14,6 +15,10 @@ const doacoesRouter = require('./src/routes/doacoes.routes.js');
 const realocacoesRoutes = require('./src/routes/realocacoes.routes.js');
 
 const app = express();
+
+// Configura CORS 
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 
 // Carrega a documentação Swagger
